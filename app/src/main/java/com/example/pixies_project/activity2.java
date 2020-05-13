512bf506deac6ddd694a3fc8a246473fabc96f1b
aryapandey <aryapandey002@gmail.com>
2020-05-13 22:14:41 +0530

    activity 2 intent+xmls

--- app/src/main/java/com/example/pixies_project/activity2.java
+++ app/src/main/java/com/example/pixies_project/activity2.java
@@ -10,15 +10,52 @@ import android.widget.Button;
 public class activity2 extends AppCompatActivity {
 
 
+    Button bmim, agebasedm, dentalm, meditationm;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity2);
+        bmim = findViewById(R.id.bmibtn);
+        dentalm = findViewById(R.id.dentalbtn);
+        agebasedm = findViewById(R.id.agebasedbtn);
+        meditationm = findViewById(R.id.meditationbtn);
+
+        bmim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),bmi.class));
+
+
+            }
+        });
+
+        agebasedm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), agebased.class));
+            }
+});
+        dentalm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),dental.class));
+
+
+            }
+        });
+        meditationm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),meditation.class));
+
+
+            }
+        });
 
 
 
 
     }
 }
-
