91f622ab101721b9c2db09fa8a77aec9a8105c55
Cheshta Sagar <cheshtas22@gmail.com>
2020-05-14 15:33:29 +0530

    bmi calculator done

--- app/src/main/java/com/example/pixies_project/MainActivity.java
+++ app/src/main/java/com/example/pixies_project/MainActivity.java
@@ -24,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         userName = findViewById(R.id.edtname);
         userAge = findViewById(R.id.age);
 
--- app/src/main/java/com/example/pixies_project/bmi.java
+++ app/src/main/java/com/example/pixies_project/bmi.java
@@ -3,12 +3,61 @@ package com.example.pixies_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class bmi extends AppCompatActivity {
+     EditText userHeight;
+     EditText userWeight;
+     Button bmiCalculate;
+     TextView result;
+    double bmi;
+    String message;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bmi);
+
+        userHeight=findViewById(R.id.height);
+        userWeight=findViewById(R.id.weight);
+        result=findViewById(R.id.bmiresult);
+        bmiCalculate=findViewById(R.id.bmiCalculate);
+        bmiCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double h=Double.parseDouble(userHeight.getText().toString());
+                double w=Double.parseDouble(userWeight.getText().toString());
+
+                 bmi= w /(h *h);
+                 checkBmi(bmi);
+                 result.setText(message);
+
+
+
+            }
+
+            void checkBmi(double a)
+            {
+                if(bmi<18.5)
+                message="UNDERWEIGHT";
+
+                else if(bmi>18.5 && bmi<24.9)
+                    message="NORMAL";
+                else
+                    message="OVERWEIGHT";
+            }
+        });
+
+
     }
+
+
+
+
+
+
 }
